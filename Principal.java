20f1822e2631ed7ee149ba30458cccfba68e1cc4
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-04-11 19:24:02 +0000

    Criando e calculando com  uma construção de objeto

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal{
+    public static void main(String[] args) {
+      
+    MinhaCalculadora calc = new MinhaCalculadora();
+        System.out.println(calc.soma(18,1));
+    
+
+    }
+}
